c30d239f34ccc99c58264575cbeb5238bfaecd3d
Darwin Delgado-Cordova <darwindel00@gmail.com>
2022-02-01 18:20:27 -0500

    adding files

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export default getNeighborhoodsList = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
