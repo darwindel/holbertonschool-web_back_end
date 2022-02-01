bbd8b4889f8f2887a93a84ac21448686200b6dc1
Darwin Delgado-Cordova <darwindel00@gmail.com>
2022-02-01 18:34:53 -0500

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
       task = true;
