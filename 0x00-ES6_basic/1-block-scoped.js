8b03d4d69c952c0fa549c2f31472af016a9ec67a
Darwin Delgado-Cordova <darwindel00@gmail.com>
2022-02-09 12:45:46 -0500

    sending in

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
-#!/usr/bin/node
-
 export default function taskBlock (trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    task === true;
-    task2 === false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,8 @@
-#!/usr/bin/node
-export default getNeighborhoodsList = () => {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
+export default function getNeighborhoodsList() {
+ let sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  add = addNeighborhood = (newNeighborhood, sanFranciscoNeighborhoods) => {
+    sanFranciscoNeighborhoods.push(newNeighborhood);
   };
 }
